953fa2d983c03284a85b8018c13aa890992a4bba
Robert Ashton <r.ashton@imperial.ac.uk>
2023-03-06 11:11:26 +0000

    Run autoformatter and fix pycodestyle issues

--- naomi_bot/app/utils.py
+++ naomi_bot/app/utils.py
@@ -1,7 +1,9 @@
 import base64
 
+
 def text_from_base64(b64_text):
-    return(bytes.decode(base64.b64decode(b64_text)))
+    return (bytes.decode(base64.b64decode(b64_text)))
+
 
 def text_to_base64(plain_text):
-    return(str(base64.b64encode(plain_text.encode()), "utf-8"))
+    return (str(base64.b64encode(plain_text.encode()), "utf-8"))
